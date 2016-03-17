a169a5825a15eeaf5904c40d2aed769df56b2781
Andres Troiano <troianoandres@gmail.com>
2016-03-16 21:30:43 -0300

    [Andres Troiano] - update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,19 @@ var exec = require('child_process').exec;
 var del = require('del');
 var runSequence = require('run-sequence');
 
+gulp.task('release:update', function (cb) {
+    exec('npm version patch', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
 gulp.task('release:move-files', function () {
     return gulp.src(['./package.json', './README.md']).pipe(gulp.dest('./aliasify-mocks'));
 });
 
-gulp.task('release:publish', function () {
+gulp.task('release:publish', function (cb) {
     exec('npm publish ./aliasify-mocks', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -22,5 +30,5 @@ gulp.task('release:clean', function (cb) {
 });
 
 gulp.task('release', function (cb) {
-    runSequence('release:move-files', 'release:publish', 'release:clean', cb);
+    runSequence('release:update', 'release:move-files', 'release:publish', 'release:clean', cb);
 });
